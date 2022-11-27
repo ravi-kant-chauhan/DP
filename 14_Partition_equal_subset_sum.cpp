8bb4c12ec711ef2517d240c188da2090a9bd9dbc
Ravi <ravkchauhan1aip@gmail.com>
2022-11-28 00:44:32 +0530

    Updated

--- 14_Partition_equal_subset_sum.cpp
+++ 14_Partition_equal_subset_sum.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// We are given an array ‘ARR’ with N positive integers. We need to find if we can partition the array into 
+// two subsets such that the sum of elements of each subset is equal to the other.
+// If we can partition, return true else return false
+
+
 
 // Memoization
 
--- 15_Partition_Set_Into_2_Subsets_With_Min_abs_Sum_Diff.cpp
+++ 15_Partition_Set_Into_2_Subsets_With_Min_abs_Sum_Diff.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// We are given an array ‘ARR’ with N positive integers. We need to partition the array into two subsets such
+// that the absolute difference of the sum of elements of the subsets is minimum.
+// We need to return only the minimum absolute difference of the sum of elements of the two partitions.
+
+
 // Memoization
 
 /*
