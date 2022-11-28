c4bc31a9fca4139074063ff60704520b720c9b0d
Ravi <ravkchauhan1aip@gmail.com>
2022-11-28 19:08:24 +0530

    Updates

--- 16_Count_Subsets_with_Sum_K.cpp
+++ 16_Count_Subsets_with_Sum_K.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// We are given an array ‘ARR’ with N positive integers and an integer K.
+//  We need to find the number of subsets whose sum is equal to K.
+
+
+
 // Memoization
 
 
@@ -129,3 +134,44 @@ int main() {
                                  
   cout<<"The number of subsets found are " <<findWays(arr,k);
 }
+
+
+// For Element 0
+/*
+int findWaysUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+
+    if(ind == 0){
+            if(target==0 && arr[0]==0)
+                return 2;
+            if(target==0 || target == arr[0])
+                return 1;
+            return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = findWaysUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= notTaken + taken;
+}
+
+int findWays(vector<int> &num, int k){
+    int n = num.size();
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    return findWaysUtil(n-1,k,num,dp);
+}
+
+int main() {
+
+  vector<int> arr = {0,0,1};
+  int k=1;
+                                 
+  cout<<"The number of subsets found are " <<findWays(arr,k);
+}
+
+*/
--- 17_Count_Partitions_with_Given_Difference.cpp
+++ 17_Count_Partitions_with_Given_Difference.cpp
@@ -1,48 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-// Tabulation
-
-
-/*
-int findWaysUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
-
-    if(ind == 0){
-            if(target==0 && arr[0]==0)
-                return 2;
-            if(target==0 || target == arr[0])
-                return 1;
-            return 0;
-    }
-    
-    if(dp[ind][target]!=-1)
-        return dp[ind][target];
-        
-    int notTaken = findWaysUtil(ind-1,target,arr,dp);
-    
-    int taken = 0;
-    if(arr[ind]<=target)
-        taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
-        
-    return dp[ind][target]= notTaken + taken;
-}
-
-int findWays(vector<int> &num, int k){
-    int n = num.size();
-    vector<vector<int>> dp(n,vector<int>(k+1,-1));
-    return findWaysUtil(n-1,k,num,dp);
-}
-
-int main() {
-
-  vector<int> arr = {0,0,1};
-  int k=1;
-                                 
-  cout<<"The number of subsets found are " <<findWays(arr,k);
-}
-
-*/
+// We are given an array ‘ARR’ with N positive integers and an integer D. We need to count the number
+//  of ways we can partition the given array into two subsets, S1 and S2 such that S1 – S2 = D and S1
+//   is always greater than or equal to S2.
 
 
 // Memoization
@@ -101,6 +62,11 @@ int main() {
 
 */
 
+// Tabulation
+
+
+/*
+
 int mod =(int)1e9+7;
 
 int findWays(vector<int> &num, int tar){
@@ -111,7 +77,6 @@ int findWays(vector<int> &num, int tar){
     if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
     else dp[0][0] = 1;  // 1 case - not pick
     
-
     if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
     
     for(int ind = 1; ind<n; ind++){
@@ -127,6 +92,62 @@ int findWays(vector<int> &num, int tar){
         }
     }
     return dp[n-1][tar];
+}
+
+int countPartitions(int n, int d, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<n;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;
+    
+    return findWays(arr,(totSum-d)/2);
+}
+  
+
+int main() {
+
+  vector<int> arr = {5,2,6,4};
+  int n = arr.size();
+  int d=3;
+                                 
+  cout<<"The number of subsets found are " <<countPartitions(n,d,arr);
+}
+
+*/
+
+
+
+// Optimized
+
+int mod =(int)1e9+7;
+
+int findWays(vector<int> &num, int tar){
+     int n = num.size();
+
+    vector<int> prev(tar+1,0);
+    
+    if(num[0] == 0) prev[0] =2;  // 2 cases -pick and not pick
+    else prev[0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) prev[num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        vector<int> cur(tar+1,0);
+        for(int target= 0; target<=tar; target++){
+            int notTaken = prev[target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = prev[target-num[ind]];
+        
+            cur[target]= (notTaken + taken)%mod;
+        }
+        prev = cur;
+    }
+    return prev[tar];
 }
 
 int countPartitions(int n, int d, vector<int>& arr){
